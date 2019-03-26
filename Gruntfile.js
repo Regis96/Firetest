d1fbce70fae94481732cf93916b7bfdb81bc260d
Régis <c1297930@bb.com.br>
2019-03-26 17:32:14 -0300

    Grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        jshint: {
+            options: {
+                esversion: 6
+            },
+            js: {
+                src: ['src/**/*.js']
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    livereload: true,
+                    middleware: function (connect, options, middlewares) {
+                        // inject a custom middleware
+                        middlewares.unshift(function (req, res, next) {
+                            res.setHeader('Access-Control-Allow-Origin', '*');
+                            res.setHeader('Access-Control-Allow-Methods', '*');
+                            return next();
+                        });
+
+                        return middlewares;
+                    },
+                    open: true
+                }
+            }
+        },
+        watch: {
+            files: ['**/*.js', '**/*.css', '**/*.html'],
+            tasks: ['build'],
+            options: {
+                livereload: true
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+
+    grunt.registerTask('default', ['jshint', 'connect', 'watch']);
+    grunt.registerTask('build', ['jshint']);
+};
